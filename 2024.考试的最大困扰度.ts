be198360008b04442be33665de7d5c69a7f8f860
allen <272970666@qq.com>
2022-03-29 14:35:39 +0800

    0329_2024

---
+++ 2024.考试的最大困扰度.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=2024 lang=typescript
+ *
+ * [2024] 考试的最大困扰度
+ */
+
+// @lc code=start
+function maxConsecutiveAnswers(answerKey: string, k: number): number {
+  let countF = 0; // 当前窗口内F的数量
+  let countT = 0; // 当前窗口内T的数量
+  let left = 0;
+  let right = 0;
+  let max = 0;
+  while (right < answerKey.length) {
+    answerKey[right] === "T" ? countT++ : countF++;
+    right++;
+    // 保证当前可以满足k次交互允许的最大窗口大小
+    while (countF > k && countT > k) {
+      answerKey[left] === "T" ? countT-- : countF--;
+      left++;
+    }
+    max = Math.max(max, countF + countT);
+  }
+  return max;
+}
+// @lc code=end
+
+const answerKey = "TTFTFFFTTTT",
+  k = 2;
+console.log(maxConsecutiveAnswers(answerKey, k));
