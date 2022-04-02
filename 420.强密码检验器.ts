d46c7c886bad18834bf812805c330c5e18f04b33
allen <272970666@qq.com>
2022-04-02 23:00:38 +0800

    0402_420_todo

---
+++ 420.强密码检验器.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=420 lang=typescript
+ *
+ * [420] 强密码检验器
+ */
+
+// @lc code=start
+function strongPasswordChecker(password: string): number {
+  const l = password.length;
+  //TODO 大致思路是根据题目的要求，把密码按长度分为三类，然后根据三种情况分别分析，目前有点菜还分析不出来，以后nb了再来搞定它
+  return;
+}
+// @lc code=end
+
+let password = "aaaaaaaaa";
+console.log(strongPasswordChecker(password));
