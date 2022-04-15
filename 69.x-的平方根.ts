695aaa550b73f0d5d3fc30ba70cbcf3465e7b791
allen <272970666@qq.com>
2022-04-16 03:14:21 +0800

    'update'

---
+++ 69.x-的平方根.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=typescript
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+function mySqrt(x: number): number {
+  return Math.floor(Math.sqrt(x));
+}
+// @lc code=end
+
+let x = 8;
+console.log(mySqrt(x));
