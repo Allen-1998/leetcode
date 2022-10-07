fd6aea3c6f3a2fb90838ca6129fea1dc79363e8e
allen <272970666@qq.com>
2022-10-08 01:54:35 +0800

    1008_870

--- 870.优势洗牌.ts
+++ 870.优势洗牌.ts
@@ -9,15 +9,24 @@ function advantageCount(nums1: number[], nums2: number[]): number[] {
   nums1.sort((a, b) => a - b);
   const r: number[] = [];
   for (let i = 0; i < nums2.length; i++) {
-    const v = nums2[i];
-    let a = nums1.findIndex((j) => j > v);
-    if (a === -1) {
-      a = nums1.findIndex((i) => i !== -1);
+    let a = -1,
+      b = -1;
+    for (let j = 0; j < nums1.length; j++) {
+      if (nums1[j] > nums2[i]) {
+        a = j;
+        break;
+      }
+      if (b === -1 && nums1[j] !== -1) {
+        b = j;
+      }
     }
+    a === -1 && (a = b);
     r.push(nums1[a]);
     nums1[a] = -1;
   }
   return r;
 }
 // @lc code=end
+// console.log(advantageCount([2, 7, 11, 15], [1, 10, 4, 11]));
+// console.log(advantageCount([12, 24, 8, 32], [13, 25, 32, 11]));
 console.log(advantageCount([2, 7, 11, 15], [1, 10, 4, 11]));
