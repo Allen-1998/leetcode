b7cf875c27500b5d01dd1bb9c3573270bf657cb8
allen <272970666@qq.com>
2022-10-17 00:00:01 +0800

    update

--- 349.两个数组的交集.ts
+++ 349.两个数组的交集.ts
@@ -6,11 +6,6 @@
 
 // @lc code=start
 function intersection(nums1: number[], nums2: number[]): number[] {
-  const m = new Map();
-  for (let i = 0; i < nums1.length; i++) {
-    const v = nums1[i];
-    m.set(v, (m.get(v) || 0) + 1);
-  }
   return [...new Set(nums1.filter((i) => nums2.includes(i)))];
 }
 // @lc code=end
